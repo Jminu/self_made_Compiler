a767c977c839f3168144dd88d19588d0459880d1
Jin Minu <s9430939@naver.com>
2025-05-11 22:51:53 +0900

    1. get_input_symbol() -> 사용자가 입력한 심볼 어떤 종류인지 2. 파일 모듈화

---
+++ src/input_symbol.c
@@ -0,0 +1,40 @@
+//
+// input_symbol 관련
+//
+#include "../headers/input_symbol.h"
+#include <ctype.h>
+
+int get_input_symbol(char ch)
+{
+    if (isalpha(ch) == 1 || ch == '_') // 알파벳이면
+        return LETTER;
+
+    if (isdigit(ch) == 1) // 숫자면
+        return DIGIT;
+
+    switch (ch) //그 외 기호면
+    {
+    case '+': return PLUS;
+    case '-': return MINUS;
+    case '*': return STAR;
+    case '/': return SLASH;
+    case '%': return PERCENT;
+    case '!': return BANG;
+    case '=': return EQUAL;
+    case '&': return AMPERSAND;
+    case '|': return VERTICAL;
+    case '(': return OPEN_PAREN;
+    case ')': return CLOSE_PAREN;
+    case '{': return OPEN_BRACE;
+    case '}': return CLOSE_BRACE;
+    case '[': return OPEN_BRACKET;
+    case ']': return CLOSE_BRACKET;
+    case ';': return SEMICOLON;
+    case ',': return COMMA;
+    case ' ': return WHITESPACE;
+    case '\0': return SYMBOL_EOF;
+    }
+
+    //모르는 기호면
+    return UNKNOWN;
+}
